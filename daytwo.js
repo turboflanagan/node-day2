b6caeca8f0a1ae7ba800acd1d8e22c6de7d42d7d
turboflanagan <turboflanagan@gmail.com>
2015-12-03 12:07:32 -0500

    add pages
    home, contact, about

--- daytwo.js
+++ daytwo.js
@@ -4,24 +4,48 @@ var url = require('url');
 var fs = require('fs');
 // var homePageHtml = fs.readFileSync('home-page.html');
 
-function homePage(request, response){
+function homeContent(){
+	var html = "<h1>This is the home page</h1>";
+	return html;
+}
+
+function aboutContent(){
+	var html = "<h1>Becoming a Builder of Things!</h1>";
+	return html;
+}
+
+function contactContent(){
+	var html = "<p>Peter Flanagan</p><br>";
+	html += "<p>Cell: 404-319-9715</p><br>";
+	html += "<p>Email: turboflanagan@gmail.com</p><br>";
+	html += "<p>website: www.turboflanagan.com</p>";
+	return html;
+}
+
+function createPage(request, response, page){
 	response.writeHead(200, {
 			'content-type': 'text/html'
 		});
-		response.end('<h1>HOME PAGE</h1>');
-}
+	if(page == 'home'){
+		response.end(homeContent());
+	}else if(page == 'about'){
+		response.end(aboutContent());
+	}else if(page == 'contact'){
+		response.end(contactContent());
+	}
+};
 
 var server = http.createServer(function(request, response){
 	var pathName = url.parse(request.url)
-	if(pathName.pathname = '/'){
+	if(pathName.pathname == '/'){
 		//User wants to see the home page
-		homePage(request, response);
+		createPage(request, response, 'home');
 	}else if(pathName.pathname == '/about'){
 		//the user wants to see the about page
-		response.writeHead(200, {
-			'content-type': 'text/html'
-		});
-		response.end('<h1>ABOUT PAGE</h1>');
+		createPage(request, response, 'about');
+	}else if(pathName.pathname == '/contact'){
+		//the user wants to see the about page
+		createPage(request, response, 'contact');
 	}else{
 		response.writeHead(200, {
 		'content-type': 'text/html'
